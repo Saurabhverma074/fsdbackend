52ad3ddb00f267b1495481f697155c315453e5e1
Saurabhverma074 <saurabhverma1051@gmail.com>
2025-04-08 11:02:10 +0530

    frontend

--- day6/frontend/src/components/Delete.jsx
+++ day6/frontend/src/components/Delete.jsx
@@ -4,7 +4,7 @@ const Delete = () => {
     const handledelete = async (e) => {
         e.preventDefault()
         const id = e.target.id.value
-        await axios.delete(`https://fsdbackend-fgf5.onrender.com//users/${id}`)
+        await axios.delete(`https://fsdbackend-fgf5.onrender.com/users/${id}`)
         alert('User deleted successfully')
       }
 
